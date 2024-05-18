188ba723bac82a43d5e460c539343378e145a57c
Arnav Borborah <arnavborborah1@gmail.com>
2024-05-17 23:37:59 -0400

    Create some basic domain types

--- DomainTypes.fs
+++ DomainTypes.fs
@@ -7,3 +7,22 @@ type BotConfig =
 type BotError =
     | NoEnvBotToken
     | BotStartError of string
+
+type ExerciseInfo = { UserId: string; Name: string }
+
+type SetBasedDetails =
+    { Warmup: bool; Reps: int; Weight: int }
+
+type SpeedBasedDetails = { Duration: int; Distance: int }
+
+type TimeBasedDetails = { Duration: int }
+
+type ExerciseLogDetails =
+    | SpeedDetails of SpeedBasedDetails
+    | TimeDetails of TimeBasedDetails
+    | AllSetDetails of SetBasedDetails seq
+
+type ExerciseLog =
+    { Info: ExerciseInfo
+      LogDetails: ExerciseLogDetails
+      Notes: string }
