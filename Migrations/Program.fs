5225beaca32a547adb81b3ef56ada647a180a4ec
Arnav Borborah <arnavborborah1@gmail.com>
2024-06-11 09:41:41 -0400

    Set up initial migrations

--- Migrations/Program.fs
+++ Migrations/Program.fs
@@ -1,4 +1,28 @@
-﻿[<EntryPoint>]
+﻿open System.Reflection
+
+open DbUp
+open Microsoft.Data.Sqlite
+
+[<EntryPoint>]
 let main args =
-    let connectionString = ""
-    0
+    let connectionString = "Data Source=JimBroBot.db"
+
+    use connection = new SqliteConnection(connectionString)
+    use _ = new SQLite.Helpers.SharedConnection(connection)
+
+    let upgrader =
+        DeployChanges.To
+            .SQLiteDatabase(connection.ConnectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToConsole()
+            .Build()
+
+    let result = upgrader.PerformUpgrade()
+
+    match result.Successful, result.Error with
+    | (false, err) ->
+        printfn $"ERROR: ${err}"
+        1
+    | (true, _) ->
+        printfn "Migrations successful"
+        0
