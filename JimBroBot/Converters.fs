374a532cdb03f35d2873cbadfbde26ec94a4f2e9
Arnav Borborah <arnavborborah1@gmail.com>
2024-07-16 23:29:17 -0400

    Add basic functions to read user data from database

--- JimBroBot/Converters.fs
+++ JimBroBot/Converters.fs
@@ -1,6 +1,7 @@
 module JimBroBot.Converters
 
 open JimBroBot.DomainTypes
+open JimBroBot.Data
 
 let stringToExerciseType str =
     match str with
@@ -8,3 +9,23 @@ let stringToExerciseType str =
     | "speed" -> SpeedBased
     | "time" -> TimeBased
     | _ -> failwith "Unexpected exercise type"
+
+let databaseExerciseToDomainExerciseInfo exercise =
+    let exerciseType = stringToExerciseType exercise.Type
+
+    { Name = exercise.Name
+      Type = exerciseType }
+
+let loadDataForUser (connectionString: string, discordId: string) =
+    task {
+        let! botUser = readBotUser connectionString discordId
+        let! botUserExercises = readExercisesForBotUser connectionString botUser.Id
+
+        let exerciseInfos = botUserExercises |> Seq.map databaseExerciseToDomainExerciseInfo
+
+        // TODO: Figure out way to load and combine exercise log
+        return
+            { Id = discordId
+              Exercises = exerciseInfos
+              ExerciseLog = Seq.empty }
+    }
